01bbcba8597b2e8f1aa0ecd483a9ad12ffe3ccd3
djangogirls <shkodaonebob@gmail.com>
2022-07-19 16:16:50 +0600

    timeLine

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,12 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  // { year: 2017, text: 'Started my journey', },
+  // { year: 2018, text: 'Worked as a freelance developer', },
+  // { year: 2019, text: 'Founded JavaScript Mastery', },
+  // { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my journey like Backend-End Developer', },
+  { year: 2022, text: 'Started my work in MMPro', },
+  { year: 2023, text: 'coming soon...', },
+  
 ];
